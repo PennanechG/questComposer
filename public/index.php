23cdea14fe8423099fda68d8ec4cf58aea208fea
Gaetan Pennanech <pennanech.gaetan@gmail.com>
2019-07-31 11:13:33 +0200

    Twig Quest

--- public/index.php
+++ public/index.php
@@ -1,13 +1,10 @@
 <?php
 require '../vendor/autoload.php';
 
-use App\Wcs\Hello;
-use HelloWorld\SayHello;
+$products = ['orange','pomme','banane','clémentine','cerise','mangue'];
 
-$gaetan = new Hello();
-echo $gaetan->talk() . "</br>";
-
-$wcs = new SayHello();
-echo $wcs->world();
+$loader = new Twig_Loader_Filesystem('../src/View');
+$twig = new Twig_Environment($loader,['cache'=> false]);
+echo $twig->render('index.html.twig', ['products'=> $products]);
 
 ?>
